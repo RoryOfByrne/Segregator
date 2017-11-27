3962bf8b9ddab92d0787eaed6a4620d860909ad9
Rory Byrne <roryofbyrne@gmail.com>
2017-11-27 10:40:13 +0000

    [Code/Misc] Cleaning the repo, added main function, added requirements.txt

--- src/personify_analysis.py
+++ src/personify_analysis.py
@@ -1,8 +1,7 @@
 from optparse import OptionParser
-
-import numpy as np
 import pandas as pd
 import sys
+
 from features.feature_builder import FeatureBuilder
 from features import dimensionality_reduction
 from model.mb_kmeans import MB_KM
@@ -10,26 +9,15 @@ from util import constants, log
 from util.graphing import graph_3d
 from data import tweets, preprocessing
 
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import seaborn as sns
-
 logger = log.logger
+
+REAL_TWEETS_DIR = "/home/rory/projects/personify/data/real"
 
 # parse commandline arguments
 op = OptionParser()
 op.add_option("--lsa",
-              dest="n_components", type="int", default=3,
+              dest="lsa_components", type="int", default=3,
               help="Preprocess documents with latent semantic analysis.")
-op.add_option("--no-minibatch",
-              action="store_false", dest="minibatch", default=True,
-              help="Use ordinary k-means algorithm (in batch mode).")
-op.add_option("--no-idf",
-              action="store_false", dest="use_idf", default=True,
-              help="Disable Inverse Document Frequency feature weighting.")
-op.add_option("--use-hashing",
-              action="store_true", default=False,
-              help="Use a hashing feature vectorizer")
 op.add_option("--n-features", type=int, default=10000,
               help="Maximum number of features (dimensions)"
                    " to extract from text.")
@@ -46,13 +34,18 @@ if len(args) > 0:
     op.error("this script takes no arguments.")
     sys.exit(1)
 
+def all_tweets():
+    real = tweets.load_directory(REAL_TWEETS_DIR, "~")
+
+    return pd.concat(real)
+
 def cluster():
     '''
         Cluster the samples together and produce graphs to show the result
 
     :return:
     '''
-    total_samples = tweets.all_tweets()
+    total_samples = tweets.all_tweets(REAL_TWEETS_DIR)
 
     # Filter unwanted rows and remove unwanted tokens
     total_samples = preprocessing.clean(total_samples)
@@ -62,15 +55,14 @@ def cluster():
     real_k = len(labels['label'].unique())
 
     # Create features from samples
-    X = total_samples.drop(['label', 'real'], axis=1)
-
+    X = total_samples.drop(['label'], axis=1)
     feature_builder = FeatureBuilder(X, constants.WORD_N, constants.CHAR_N)
     X = feature_builder.featurize_all(X)
 
     logger.debug("n_samples: %d, n_features: %d" % X.shape)
 
     # Reduce domensionality (Curse of Dimensionality)
-    reducer = dimensionality_reduction.LSA(opts.n_components)
+    reducer = dimensionality_reduction.LSA(opts.lsa_components)
     X = reducer.reduce(X)
 
     logger.debug("After LSA shape: %d samples, %d features" % X.shape)
@@ -84,5 +76,10 @@ def cluster():
 
     reducer.common_terms(model, model.feature_builder.word_pres_vec, real_k)
 
-cluster()
+def main():
+    cluster()
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
 
